65c228851c6c31bd656a1c6ad98fa2834895e603
amecea <flavius@presslabs.com>
2020-04-13 16:56:32 +0300

    Use dashboard-installer cluster role binding in GCP

--- scripts/check_files.sh
+++ scripts/check_files.sh
@@ -9,7 +9,6 @@ filesinKustomizeFile=.build/files_from_kustomize.txt
 
 find $dir_path -name '*.yaml' |\
     grep -v 'controller-clusterrole-kubebuilder.yaml' |\
-    grep -v 'dashboard-installer.yaml' |\
     sed 's/.build\/manifest/../g' | sort > $chartFile
 
 echo -n "" > $filesinKustomizeFile.tmp
