be5e8b05a891933ee63f8c2f3551cadfc8a5d1e6
Robert Stefan <rstefan1@users.noreply.github.com>
2021-02-05 12:27:50 +0200

    Remove leftover (OIDC secret)

--- scripts/check_files.sh
+++ scripts/check_files.sh
@@ -10,8 +10,6 @@ filesinKustomizeFile=.build/files_from_kustomize.txt
 # lists files from chart and filter them
 find $dir_path -name '*.yaml' |\
     grep -v 'controller-clusterrole-kubebuilder.yaml' |\
-    grep -v 'apiserver-oidc-secret.yaml' |\
-    grep -v 'apiserver-sentry-secret.yaml' |\
     sed 's/.build\/manifest/../g' | sort > $chartFile
 
 echo -n "" > $filesinKustomizeFile.tmp
